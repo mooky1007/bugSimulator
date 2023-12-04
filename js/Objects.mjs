ed05bb0da94757241828147fd7427ff2818b664a
mooky1007 <mooky1007@naver.com>
2023-12-04 12:37:05 +0900

    update: 피식자는 더이상 자신보다 작은 포식자로부터 도망치지 않음

--- js/Objects.mjs
+++ js/Objects.mjs
@@ -332,7 +332,9 @@ export class Bug extends Objects {
         }
 
         if (this.predator.length > 0) {
-            this.predator.sort((a, b) => {
+            this.predator
+            .filter((tile) => tile?.content?.size >= this.size)
+            .sort((a, b) => {
                 const aDistance = Math.sqrt(Math.pow(a.x - this.position.x, 2) + Math.pow(a.y - this.position.y, 2));
                 const bDistance = Math.sqrt(Math.pow(b.x - this.position.x, 2) + Math.pow(b.y - this.position.y, 2));
                 return aDistance - bDistance;
